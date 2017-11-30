f1e739d373a1c77254def90e55ef244205ec576f
nishuai <446332253@qq.com>
2017-11-30 12:26:05 +0800

    2.0

---
+++ src/main/java/com/nsproject/girl/HelloController.java
@@ -0,0 +1,38 @@
+package com.nsproject.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Top")
+public class HelloController {
+
+//    @Value("${cupSize}")
+//    private String cupSize;
+//
+//    @Value("${age}")
+//    private Integer age;
+//
+//    @Value("${context}")
+//    private String context;
+
+    @Autowired
+    private GirlPropertise girlp;
+
+    //@RequestMapping(value = {"/hello","/hi"}, method = RequestMethod.GET)
+    @GetMapping("/hello")
+    //@RequestMapping(value = {"/hello/{id}"}, method = RequestMethod.GET)
+    public String say(@RequestParam(value = "id", defaultValue = "0",required = false) Integer id){
+                        //@PathVariable("id") Integer id
+        //return cupSize+age;
+       // return  "Hellow Spring Boot!";
+       // return girlp.getCupSize();
+        return "id:"+id;
+    }
+
+
+
+}
